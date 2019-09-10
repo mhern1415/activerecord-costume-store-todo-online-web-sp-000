e3ac45c0c80501b3e314323e0b1e4a2eae1ab267
Mark Hernandez <m.hern1415@gmail.com>
2019-09-10 15:11:49 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,4 +10,11 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
   
-  
+  def change
+    create_table :candies do |t|
+      t.string :name
+      t.integer :calories
+      t.timestamps
+    end
+  end
+end
